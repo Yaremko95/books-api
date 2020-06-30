d53ee5c7bc137c6259154025ab530f9a7367e506
tetiana <tetianayaremko@gmail.com>
2020-06-30 16:48:13 +0200

    script

--- src/server.js
+++ src/server.js
@@ -4,7 +4,19 @@ const booksRoutes = require('./books')
 const cors = require ('cors')
 const app = express()
 
-app.use(cors())
+
+const whitelist = ["http://localhost:3000", "http://localhost:3002"]
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+}
+app.use(cors(corsOptions))
 
 app.use(express.json())
 const port = process.env.PORT ||3004
